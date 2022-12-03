43f763b4324b6a01381938789f10db585764c05f
Julian Rodriguez <julianguerra92@gmail.com>
2022-12-03 12:24:42 -0500

    Creación de schema para guardar en base de datos y creación del enpoint POST para guardar Pokemones

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Definición de Pipe global en la aplicación para el uso de las validaciones o DTOS
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //limpia la información enviada en la petición
+      forbidNonWhitelisted: true //Filtra la petición y genera error en caso de no se conforme
+    })
+  )
+
   app.setGlobalPrefix('api/v2');
   await app.listen(3000);
 }
--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
 
 @Module({
   controllers: [PokemonController],
-  providers: [PokemonService]
+  providers: [PokemonService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Pokemon.name, //name hace referencia al nombre asignado por monngoose
+        schema: PokemonSchema
+      }
+    ])
+  ]
 })
-export class PokemonModule {}
+export class PokemonModule { }
